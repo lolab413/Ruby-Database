a540b074f2f7c1f67cdf72edcb37f6d1a96c2200
lolab413 <lolababatunde413@gmail.com>
2021-08-24 06:48:01 -0500

    first commit

---
+++ db/schema.rb
@@ -0,0 +1,13 @@
+ActiveRecord::Schema.define(version: 2021_08_22_202002) do
+
+  create_table "friends", force: :cascade do |t|
+    t.string "first_name"
+    t.string "last_name"
+    t.string "email"
+    t.string "phone"
+    t.string "address"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+end
